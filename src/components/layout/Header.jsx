5dddd32fe5b8d47d3104ad82af50575ff000b42d
Dikeh Daniel <dikehdaniel2020@gmail.com>
2025-10-21 22:21:43 -0700

    fixed and remodelled the nav and sidebar NavLinks

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Search, Calendar, Mail, Bell, ChevronDown } from 'lucide-react';
 
 export default function Header() {
